e8a88d92f8cef15266c76f34c36ac4e5ae4607c6
Stanisław Horna <stanislawhorna@outlook.com>
2024-07-06 18:49:35 +0200

    add registration handling for repo-related runners

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ export RUNNER_ALLOW_RUNASROOT="0"
 
 RUNNER_LABELS="container,$LABELS"
 
-if [ "$ORGANIZATION" = "xxx" ]; then
-    echo "Missing organization name"
+if [ "$GITHUB_OBJECT" = "xxx" ]; then
+    echo "Missing GitHub object name"
     exit 1
 fi;
 
@@ -20,13 +20,17 @@ else
     RUNNER_LABELS="container,$LABELS"
 fi;
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token" | jq .token --raw-output)
+if echo "$GITHUB_OBJECT" | grep -q "/" ; then
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/repos/${GITHUB_OBJECT}/actions/runners/registration-token" | jq .token --raw-output)
+else
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/orgs/${GITHUB_OBJECT}/actions/runners/registration-token" | jq .token --raw-output)
+fi;
 
 cd /usr/bin/actions-runner || exit 3
 
 echo "$REG_TOKEN" > token.txt
 
-./config.sh --url "https://github.com/${ORGANIZATION}" --token "$REG_TOKEN"  --labels "$RUNNER_LABELS"
+./config.sh --url "https://github.com/${GITHUB_OBJECT}" --token "$REG_TOKEN"  --labels "$RUNNER_LABELS"
 
 cleanup() {
     echo "Removing runner..."
